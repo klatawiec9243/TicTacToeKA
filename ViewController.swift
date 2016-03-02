b7e1e02fc7c60bf4364ebb9cc66d74714a18a14f
klatawiec9243 <klatawiec9243@stu.d214.org>
2016-03-02 07:39:46 -0600

    .
    .

--- ViewController.swift
+++ ViewController.swift
@@ -8,11 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    @IBOutlet weak var one: UILabel!
+    @IBOutlet weak var two: UILabel!
+    @IBOutlet weak var three: UILabel!
+    @IBOutlet weak var four: UILabel!
+    @IBOutlet weak var five: UILabel!
+    @IBOutlet weak var six: UILabel!
+    @IBOutlet weak var seven: UILabel!
+    @IBOutlet weak var eight: UILabel!
+    @IBOutlet weak var nine: UILabel!
+    @IBOutlet weak var currentPlayerLabel: UILabel!
+    
+    var plays: [String:Int] = [
+        "X":5,
+        "O":1
+    ]
+    
+    var letters = "XOXOXOXOXOXOXOXOXO"
+    var counter = 0
+    var currentLetter: Character!
+    var labelArray = [UILabel()]
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        labelArray = [one, two, three, four, five, six, seven, eight, nine]
+        getAndSetCurrentLetter()
     }
-
-}
+    
+    @IBAction func whenTapped(sender: UITapGestureRecognizer)
+    {
+        var selectedPoint = sender.locationInView(self.view)
+        for label in labelArray {
+            if CGRectContainsPoint(label.frame, selectedPoint)
+            {
+                label.text = String(currentLetter)
+            }
+        }
+        counter++
+        if counter == 9
+        {
+            counter = resetGame()
+        }
+        getAndSetCurrentLetter()
+    }
+    
+    func getAndSetCurrentLetter()
+    {
+        currentLetter = letters[letters.startIndex.advancedBy(counter)]
+        currentPlayerLabel.text = String(currentLetter)
+    }
+    
+    func resetGame() ->Int
+    {
+        let alert = UIAlertController(title: "Reseting Game", message: "To play again, tap OK", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler:
+            {
+                sender in
+                self.one.text = ""
+                self.two.text = ""
+                self.three.text = ""
+                self.four.text = ""
+                self.five.text = ""
+                self.six.text = ""
+                self.seven.text = ""
+                self.eight.text = ""
+                self.nine.text = ""
+        })
+        alert.addAction(okAction)
+        presentViewController(alert, animated: true, completion: nil)
+        return 0
+    }
+    
+    func win()
+    {
+        
+    }
